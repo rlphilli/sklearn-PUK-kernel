ded7b4120c4cbc54fd990140e9efd162d0f5945c
h205c <h205c@musk.cs.haverford.edu>
2017-02-08 23:57:52 -0500

    Initial commit

---
+++ PUK_kernel.py
@@ -0,0 +1,23 @@
+
+
+
+import numpy as np
+from scipy.spatial.distance import pdist, cdist
+from scipy.spatial.distance import squareform
+
+
+def PUK_kernel(X1,X2, sigma=1.0, omega=1.0):
+    # Compute the kernel matrix between two arrays using the Pearson VII function-based universal kernel.
+
+    # Compute squared euclidean distance between each row element pair of the two matrices
+    if X1 is X2 :
+        kernel = squareform(pdist(X1, 'sqeuclidean'))
+    else:
+        kernel = cdist(X1, X2, 'sqeuclidean')
+
+    kernel = (1 + (kernel * 4 * np.sqrt(2**(1.0/omega)-1)) / sigma**2) ** omega
+    kernel = 1/kernel
+
+    return kernel
+
+
